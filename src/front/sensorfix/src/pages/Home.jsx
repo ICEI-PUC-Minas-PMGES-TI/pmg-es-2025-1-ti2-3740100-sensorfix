f849c1af07d5421af721dc33bcda4e1f368bdb50
João Marcos de Aquino Gonçalves <1336608@sga.pucminas.br>
2025-04-08 19:46:00 -0300

    alter: home page

--- src/front/sensorfix/src/pages/Home.jsx
+++ src/front/sensorfix/src/pages/Home.jsx
@@ -1,47 +1,23 @@
 import React from 'react';
-
+import { FaTools } from 'react-icons/fa';
+import { MdPrecisionManufacturing } from "react-icons/md";
+import { BsBoxSeam } from "react-icons/bs";
 export default function Home() {
     return (
-        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-            <h1 style={{ textAlign: 'center' }}>Bem-vindo à Home</h1>
-            <p style={{ textAlign: 'center' }}>
-                Este é um teste de responsividade. Redimensione a janela para verificar o comportamento.
-            </p>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
-                <div style={{
-                    flex: '1 1 300px',
-                    minWidth: '200px',
-                    maxWidth: '400px',
-                    padding: '10px',
-                    border: '1px solid #ccc',
-                    borderRadius: '8px',
-                    textAlign: 'center'
-                }}>
-                    <p>Caixa 1</p>
-                </div>
-                <div style={{
-                    flex: '1 1 300px',
-                    minWidth: '200px',
-                    maxWidth: '400px',
-                    padding: '10px',
-                    border: '1px solid #ccc',
-                    borderRadius: '8px',
-                    textAlign: 'center'
-                }}>
-                    <p>Caixa 2</p>
-                </div>
-                <div style={{
-                    flex: '1 1 300px',
-                    minWidth: '200px',
-                    maxWidth: '400px',
-                    padding: '10px',
-                    border: '1px solid #ccc',
-                    borderRadius: '8px',
-                    textAlign: 'center'
-                }}>
-                    <p>Caixa 3</p>
-                </div>
+        <>
+            <div className="container-fluid">
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <Link className="nav-link text-white" to="/producao"><MdPrecisionManufacturing /> Produção</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link className="nav-link text-white" to="/estoque"><BsBoxSeam /> Estoque</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link className="nav-link text-white" to="/manutencao"><FaTools /> Manutenção</Link>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </>
     );
 }
