2eee1d7eddac2c6cf368dfc8bd6f6ce0b5c9e956
jjoaom <joaomarcos.aquino@hotmail.com>
2025-04-08 17:28:53 -0300

    alter: pequenas alteraçoes de icon e footer

--- src/front/sensorfix/src/App.jsx
+++ src/front/sensorfix/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -5,10 +6,27 @@ import Home from "./pages/Home.jsx";
 import Producao from "./pages/Producao.jsx";
 
 export default function App() {
+  const [theme, setTheme] = useState(getPreferredTheme());
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  useEffect(() => {
+    const favicon = document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.href = theme === 'dark' ? '/favicon-dark.ico' : '/favicon-light.ico';
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Router>
-        <Header />
+        <Header theme={theme} toggleTheme={toggleTheme} />
         <div className="flex-grow-1 overflow-auto">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -19,4 +37,8 @@ export default function App() {
       </Router>
     </div>
   );
+}
+
+function getPreferredTheme() {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
